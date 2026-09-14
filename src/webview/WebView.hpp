88cf3d0ef4b27aa2eda9393a7a5f395aa68015c3
Neon <neongmd1667@gmail.com>
2026-09-14 18:42:04 +0700

    Create WebView.hpp

---
+++ src/webview/WebView.hpp
@@ -0,0 +1,18 @@
+
+#pragma once
+
+#include <string>
+
+namespace geotube::webview {
+
+    void create();
+    void destroy();
+
+    void loadURL(const std::string& url);
+    void executeJS(const std::string& js);
+
+    void setVisible(bool visible);
+    bool isVisible();
+
+}
+
